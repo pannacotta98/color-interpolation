c33c94ac317f3981e8920fde023622f1c627a9fc
Ostronkim <arvidmagnusson97@gmail.com>
2022-02-14 13:51:48 +0100

    Remove image output and return mean error

--- src/scielab/calcScielab.m
+++ src/scielab/calcScielab.m
@@ -1,8 +1,10 @@
-function calcScielab(org_img, interp_img)
+function meanError = calcScielab(org_img, interp_img)
 % File to calculate s-cielab value of an image and its interpolated version
 %inputs:
 %   org_img is the original image in rgb
 %   interp_img is the interpolated version of original image
+%output
+% meanError is the mean of the error image
 
     
 %Load the calibration information
@@ -12,7 +14,7 @@ load SmithPokornyCones;
 rgb2lms = cones'* displaySPD;
 load displayGamma;
 rgbWhite = [1 1 1];
-whitepoint = rgbWhite * rgb2lms'
+whitepoint = rgbWhite * rgb2lms';
     
 % Convert the RGB data to LMS.
     
@@ -23,26 +25,26 @@ imageformat = 'lms';
 % Run the scielab function
 errorImage = scielab(sampPerDeg, img1LMS, img2LMS, whitepoint, imageformat);
 %Get the mean scielab difference
-mean_error = mean(errorImage(:))
+meanError = mean(errorImage(:));
     
     
 errorTruncated = min(128*(errorImage/10),128*ones(size(errorImage)));
-figure(1)
-colormap(gray(128));
-image(errorTruncated); axis image;
+%figure(1)
+%colormap(gray(128));
+%image(errorTruncated); axis image;
     
 edgeImage = 129 * double(edge(org_img(:,:,1),'prewitt'));
     
 comparison = max(edgeImage,errorTruncated);
 mp = [gray(127); [0 1 0]; [1 0 0] ];
-colormap(mp)
-image(comparison)
+%colormap(mp)
+%image(comparison)
     
-figure(2)
-imshow(org_img)
+%figure(2)
+%imshow(org_img)
     
-figure(3)
-imshow(interp_img)
+%figure(3)
+%imshow(interp_img)
 
 end
 
