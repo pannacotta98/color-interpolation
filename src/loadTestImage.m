588b540fc93a64224b546afcbccfd88c8cffb7ce
Algot Sandahl <algot.sandahl@gmail.com>
2022-02-17 16:54:00 +0100

    Make loadTestImage dialog larger

--- src/loadTestImage.m
+++ src/loadTestImage.m
@@ -4,7 +4,8 @@ jpegList = dir('./../data/**/*.jpeg');
 jpgList = dir('./../data/**/*.jpg');
 
 list = vertcat(pngList, jpgList, jpegList);
-[indx,tf] = listdlg('ListString',{list.('name')}, 'SelectionMode','single');
+[indx,tf] = listdlg('ListString',{list.('name')}, ...
+    'SelectionMode','single', 'ListSize',[500,500]);
 if tf == 0
     error('No image selected');
 end
