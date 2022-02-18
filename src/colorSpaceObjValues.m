3a7f9f4b4039ce19f56470c891ac553b43363ad2
Algot Sandahl <algot.sandahl@gmail.com>
2022-02-18 09:26:47 +0100

    Store intermediate steps in colorSpaceObjValues

--- src/colorSpaceObjValues.m
+++ src/colorSpaceObjValues.m
@@ -5,28 +5,28 @@ img = loadTestImage();
 scalefactor = 4;
 
 %Downscale
-img_srgb = upscaleInColorSpace(img,1/scalefactor,'srgb','lanczos2');
-img_lin = upscaleInColorSpace(img,1/scalefactor,'linrgb','lanczos2');
-img_hsv = upscaleInColorSpace(img,1/scalefactor,'hsv','lanczos2');
-img_ycbcr = upscaleInColorSpace(img,1/scalefactor,'ycbcr','lanczos2');
-img_lab = upscaleInColorSpace(img,1/scalefactor,'lab','lanczos2');
-img_xyz = upscaleInColorSpace(img,1/scalefactor,'xyz','lanczos2');
+img_srgb_ds = upscaleInColorSpace(img,1/scalefactor,'srgb','lanczos2');
+img_lin_ds = upscaleInColorSpace(img,1/scalefactor,'linrgb','lanczos2');
+img_hsv_ds = upscaleInColorSpace(img,1/scalefactor,'hsv','lanczos2');
+img_ycbcr_ds = upscaleInColorSpace(img,1/scalefactor,'ycbcr','lanczos2');
+img_lab_ds = upscaleInColorSpace(img,1/scalefactor,'lab','lanczos2');
+img_xyz_ds = upscaleInColorSpace(img,1/scalefactor,'xyz','lanczos2');
 
 %Clamp
-img_srgb = clamp(img_srgb,0,1);
-img_lin = clamp(img_lin,0,1);
-img_hsv = clamp(img_hsv,0,1);
-img_ycbcr = clamp(img_ycbcr,0,1);
-img_lab = clamp(img_lab,0,1);
-img_xyz = clamp(img_xyz,0,1);
+img_srgb_ds_clamped = clamp(img_srgb_ds,0,1);
+img_lin_ds_clamped = clamp(img_lin_ds,0,1);
+img_hsv_ds_clamped = clamp(img_hsv_ds,0,1);
+img_ycbcr_ds_clamped = clamp(img_ycbcr_ds,0,1);
+img_lab_ds_clamped = clamp(img_lab_ds,0,1);
+img_xyz_ds_clamped = clamp(img_xyz_ds,0,1);
 
 %Upscale
-img_srgb = upscaleInColorSpace(img_srgb,scalefactor,'srgb','ogniewski');
-img_lin = upscaleInColorSpace(img_lin,scalefactor,'linrgb','ogniewski');
-img_hsv = upscaleInColorSpace(img_hsv,scalefactor,'hsv','ogniewski');
-img_ycbcr = upscaleInColorSpace(img_ycbcr,scalefactor,'ycbcr','ogniewski');
-img_lab = upscaleInColorSpace(img_lab,scalefactor,'lab','ogniewski');
-img_xyz = upscaleInColorSpace(img_xyz,scalefactor,'xyz','ogniewski');
+img_srgb = upscaleInColorSpace(img_srgb_ds_clamped,scalefactor,'srgb','ogniewski');
+img_lin = upscaleInColorSpace(img_lin_ds_clamped,scalefactor,'linrgb','ogniewski');
+img_hsv = upscaleInColorSpace(img_hsv_ds_clamped,scalefactor,'hsv','ogniewski');
+img_ycbcr = upscaleInColorSpace(img_ycbcr_ds_clamped,scalefactor,'ycbcr','ogniewski');
+img_lab = upscaleInColorSpace(img_lab_ds_clamped,scalefactor,'lab','ogniewski');
+img_xyz = upscaleInColorSpace(img_xyz_ds_clamped,scalefactor,'xyz','ogniewski');
 
 
 colorSpace = {'SRGB';'LINRGB';'HSV';'YCBCR';'CIELAB';'XYZ'};
