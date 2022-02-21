58f4fb9127a3a3b6b802d47273727d0d5af7c8b1
Ostronkim <arvidmagnusson97@gmail.com>
2022-02-21 16:00:07 +0100

    Create boxplots

---
+++ src/boxPlot.m
@@ -0,0 +1,17 @@
+load('../data/objValues.mat');
+
+figure
+boxplot(SSIM,'Labels',{'sRGB','Lin RGB','HSV','YCbCr','CIELAB','XYZ'})
+ylabel('SSIM index')
+title('SSIM value for the color spaces')
+
+figure
+boxplot(PSNR,'Labels',{'sRGB','Lin RGB','HSV','YCbCr','CIELAB','XYZ'})
+ylabel('PSNR value')
+title('PSNR value for the color spaces')
+
+figure
+boxplot(SCIELAB,'Labels',{'sRGB','Lin RGB','HSV','YCbCr','CIELAB','XYZ'})
+ylabel('Mean error')
+title('s-CIELAB value for the color spaces')
+
