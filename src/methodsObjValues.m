20021fca8cc52785f00187fcbccd39b2208aa374
Ostronkim <arvidmagnusson97@gmail.com>
2022-02-28 14:27:47 +0100

    calculate for every image and store values

--- src/methodsObjValues.m
+++ src/methodsObjValues.m
@@ -1,38 +1,48 @@
+clear
 addpath('./scielab')
 addpath('./ogniewski')
 addpath('../data/color-space-eval/')
-img = loadTestImage();
 
+png = dir('./../data/color-space-eval/*.png');
+jpg = dir('./../data/color-space-eval/*.jpg');
 scalefactor = 4;
 
-%Downscale
-img_ds = imresize(img,1/scalefactor,'lanczos2');
+downscaleMethod = 'lanczos2';
+upscaleMethods = {'nearest','bilinear','bicubic','lanczos3','ogniewski'}';
 
-%Upscale
-img_near = imresize(img_ds,scalefactor,'nearest');
-img_bil = imresize(img_ds,scalefactor,'bilinear');
-img_bic = imresize(img_ds,scalefactor,'bicubic');
-img_lanc = imresize(img_ds,scalefactor,'lanczos2');
-img_og = ogniewskiUpscale(img_ds, scalefactor);
+f = vertcat(jpg,png);
+files={f.name};
+for k=1:numel(files)
+    files{k}
+    Im{k}=imread(files{k});
+    img = im2double(imread(files{k}));
 
-interpMethod = {'nearest';'bilinear';'bicubic';'lanczos2';'Ogniewski'};
-SSIM = [mean(ssim(img_near,img,'DataFormat','SSC')); mean(ssim(img_bil,img,'DataFormat','SSC'));
-    mean(ssim(img_bic,img,'DataFormat','SSC')); mean(ssim(img_lanc,img,'DataFormat','SSC'));
-    mean(ssim(img_og,img,'DataFormat','SSC'));];
-PSNR = [psnr(img_near,img);psnr(img_bil,img); psnr(img_bic,img); psnr(img_lanc,img);
-        psnr(img_og,img)];
-SCIELAB = [calcScielab(img, img_near); calcScielab(img, img_bil); calcScielab(img, img_bic);
-          calcScielab(img, img_lanc); calcScielab(img, img_og)];
+    %Downscale
+    img_ds = imresize(img,1/scalefactor,downscaleMethod);
+    img_ds = clamp(img_ds, 0, 1);
+    
+    %Upscale
+    out = cell(length(upscaleMethods), 1);
+    for i=1:length(upscaleMethods)
+        upscaled = upscaleInColorSpace(img_ds, scalefactor, 'srgb', upscaleMethods{i});
+        out{i} = clamp(upscaled, 0, 1);
+    end
 
-table(interpMethod,SSIM,PSNR,SCIELAB)
+    SSIM{k} = cellfun(@(scaled) mean(ssim(scaled,img,'DataFormat','SSC')), out);
+    PSNR{k} = cellfun(@(scaled) psnr(scaled,img), out);
+    SCIELAB{k} = cellfun(@(scaled) calcScielab(scaled,img), out);
+    meanDeltaE{k} = cellfun(@(scaled) mean(deltaE(scaled, img), 'all'), out);
+    medianDeltaE{k} = cellfun(@(scaled) median(deltaE(scaled, img), 'all'), out);
+    NIQE{k} = cellfun(@(scaled) niqe(scaled),out);
+    BRISQUE{k} = cellfun(@(scaled) brisque(scaled),out);
 
-figure('Name', 'nearest');
-imshow(img_bil)
-figure('Name', 'bilinear');
-imshow(img_bil)
-figure('Name', 'bicubic');
-imshow(img_bic)
-figure('Name', 'lanczos2');
-imshow(img_lanc)
-figure('Name', 'ogniewski');
-imshow(img_og)
+    table(upscaleMethods, SSIM{k}, PSNR{k}, SCIELAB{k}, meanDeltaE{k}, medianDeltaE{k}, NIQE{k}, BRISQUE{k})
+end
+
+    SSIM = transpose(cell2mat(SSIM)); PSNR = transpose(cell2mat(PSNR)); 
+    SCIELAB = transpose(cell2mat(SCIELAB)); meanDeltaE = transpose(cell2mat(meanDeltaE));
+    medianDeltaE = transpose(cell2mat(medianDeltaE));
+
+    %Uncomment to save variables
+    %save('../data/interp-methods-eval/objValues.mat','SSIM','PSNR','SCIELAB', ...
+     %   'meanDeltaE','medianDeltaE','-append');
