0bca0363519c51266ea179f36243f045d68eadd3
Ostronkim <arvidmagnusson97@gmail.com>
2022-02-28 14:29:12 +0100

    Can plot interp methods values too

--- src/boxPlot.m
+++ src/boxPlot.m
@@ -1,17 +1,35 @@
-load('../data/color-space-eval/objValues.mat');
+%% Plot color space values
+%load('../data/color-space-eval/objValues.mat');
+
+% figure
+% boxplot(SSIM,'Labels',{'sRGB','Lin RGB','HSV','YCbCr','CIELAB','XYZ'})
+% ylabel('SSIM index')
+% title('SSIM value for the color spaces')
+% 
+% figure
+% boxplot(PSNR,'Labels',{'sRGB','Lin RGB','HSV','YCbCr','CIELAB','XYZ'})
+% ylabel('PSNR value')
+% title('PSNR value for the color spaces')
+% 
+% figure
+% boxplot(SCIELAB,'Labels',{'sRGB','Lin RGB','HSV','YCbCr','CIELAB','XYZ'})
+% ylabel('Mean error')
+% title('S-CIELAB value for the color spaces')
+
+%% Plot interpolation method values
+load('../data/interp-methods-eval/objValues.mat');
 
 figure
-boxplot(SSIM,'Labels',{'sRGB','Lin RGB','HSV','YCbCr','CIELAB','XYZ'})
+boxplot(SSIM,'Labels',{'nearest','bilinear','bicubic','lanczos3','Ogniewski'})
 ylabel('SSIM index')
-title('SSIM value for the color spaces')
+title('SSIM value for the interpolation methods')
 
 figure
-boxplot(PSNR,'Labels',{'sRGB','Lin RGB','HSV','YCbCr','CIELAB','XYZ'})
+boxplot(PSNR,'Labels',{'nearest','bilinear','bicubic','lanczos3','Ogniewski'})
 ylabel('PSNR value')
-title('PSNR value for the color spaces')
+title('PSNR value for the interpolation methods')
 
 figure
-boxplot(SCIELAB,'Labels',{'sRGB','Lin RGB','HSV','YCbCr','CIELAB','XYZ'})
+boxplot(SCIELAB,'Labels',{'nearest','bilinear','bicubic','lanczos3','Ogniewski'})
 ylabel('Mean error')
-title('s-CIELAB value for the color spaces')
-
+title('S-CIELAB value for the intepolation methods')
