db5b742ed1e0cf66cbd9836e1da800715a9947fa
Algot Sandahl <algot.sandahl@gmail.com>
2022-03-16 11:55:32 +0100

    Change file list to gui file select

--- src/loadTestImage.m
+++ src/loadTestImage.m
@@ -1,16 +1,21 @@
 function im = loadTestImage()
-pngList = dir('./../data/**/*.png');
-jpegList = dir('./../data/**/*.jpeg');
-jpgList = dir('./../data/**/*.jpg');
+% pngList = dir('./../data/**/*.png');
+% jpegList = dir('./../data/**/*.jpeg');
+% jpgList = dir('./../data/**/*.jpg');
+% 
+% list = vertcat(pngList, jpgList, jpegList);
+% [indx,tf] = listdlg('ListString',{list.('name')}, ...
+%     'SelectionMode','single', 'ListSize',[500,500]);
+% if tf == 0
+%     error('No image selected');
+% end
+% file = list(indx);
+% fileName = fullfile(file.folder, file.name);
+% disp(['Loaded ', fileName]);
+% im = im2double(imread(fileName));
 
-list = vertcat(pngList, jpgList, jpegList);
-[indx,tf] = listdlg('ListString',{list.('name')}, ...
-    'SelectionMode','single', 'ListSize',[500,500]);
-if tf == 0
-    error('No image selected');
-end
-file = list(indx);
-fileName = fullfile(file.folder, file.name);
+[file,path] = uigetfile('./../data/*');
+fileName = fullfile(path, file);
 disp(['Loaded ', fileName]);
 im = im2double(imread(fileName));
 end
